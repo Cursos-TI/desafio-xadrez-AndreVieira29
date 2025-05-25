6dbe94251971a5a608bf692ec54f8b9d99e86655
AndreVieira29 <202502883358@alunos.estacio.br>
2025-05-25 22:00:42 +0000

    acrescentado movimento da peça Bispo e Cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,54 +1,70 @@
 // Desafio de Xadrez - MateCheck
 #include <stdio.h>
 
-//movimento da torre para a direita 5 casas
+//movimento da torre para a direita 5 casas usando função recusiva
 void moveTorre(int i){
     if (i > 0 ){
     printf("Direita\n");
     moveTorre(i- 1);
     }
 }
-//movimento da rainha para esquerda 8 casas
+//movimento da rainha para esquerda 8 casas usando função recursiva
 void moveRainha(int i){
 if (i > 0 ){
     printf("Escquerda\n");
     moveRainha(i - 1);
    }
-
 }
+
+void moveCavalo() {
+    int i, j;
+    // laço aninhado para simular movimento do Cavalo no Xadrez
+    for (i = 0; i < 3; i++) {          // Laço externo imprimi "cima"
+        for (j = 0; j < 3; j++) {      // Laço interno imprimi "direita"
+
+            if (i < 2 && j == 0) {
+                printf("cima ");
+                // Após imprimir "cima" nesta iteração, uso comando "continue" para pular para a próxima.
+                continue;
+            }
+            // Apos imprimir duas vez "Cima" a condição sera verdade e ira imprimir "direita"
+            if (i == 2 && j == 1) {
+                printf("direita");
+                // Após imprimir Direita, encerra o bloco com break.
+                break;
+            }
+        }
+    }
+}
+ //simula o movimento da peça de xadrez "Bispo"
+ void moveBispo(int loopRecursivo) { 
+    int i, j;
+
+    if (loopRecursivo <= 0) {
+        return; // encerra o loop quando variavel recursiva chegar a "0"
+    }
+    //a cada loop recursivo executa uma vez o loop externo e interno como verdadeiro
+    for (i = 0; i < 1; i++) { // Loop externo
+        printf("Cima\n");
+        
+        // Loop interno
+        for (j = 0; j < 1; j++) { // Loop interno
+            printf("Direita\n");
+        }
+    }
+    //subtrai -1 do contador recursivo
+    moveBispo(loopRecursivo - 1);
+}
+
 int main(){
 
-     /*MOVIMENTOS PEÇA DA TORRE USANDO DO WHILE
-     do{
-        printf("Direita\n");
-        movimentaTorre--;
-       }
-    while(movimentaTorre > 0);
-
-    //MOVER BISPO PARA DIAGONAL UTILIZANDO WHILE
-    while(movimentaBispo > 0){
-        printf("Cima, Direita\n");
-        movimentaBispo--;
-        }
-     
-    //MOVER PEÇA RAINHA  UTILIZANDO FOR
-    for(movimentaRainha=8; movimentaRainha > 0;movimentaRainha--){
-        printf("Esquerda\n");
-        }
-    //MOVER PEÇA CAVALO USANDO LOOP ANIMHADO
-    printf("\n");
-
-    for(movimentaCavalo = 2 ;movimentaCavalo > 0; movimentaCavalo--){
-         while (movimentaCavalo > 0){
-            printf("baixo\n");  
-            movimentaCavalo--;
-         }
-         printf("Esquerda\n");
-        }
-    */
+   //chamada da funcões para imprimir o movimeto das peças de Xadres 
     moveTorre(5);
+    printf("\n");
+    moveBispo(5);
     printf("\n");
     moveRainha(8);
     printf("\n");
+    moveCavalo();
     return 0;
   } 
